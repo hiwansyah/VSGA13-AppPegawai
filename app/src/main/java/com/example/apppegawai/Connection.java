9c4f5fd7cb68b54bc365d7e271cde0cce4f5e0d6
hiwansyah <iwansyy@gmail.com>
2020-10-14 21:37:50 +0700

    first commit

---
+++ app/src/main/java/com/example/apppegawai/Connection.java
@@ -0,0 +1,12 @@
+package com.example.apppegawai;
+
+public class Connection {
+    public static String BASE_URL="http://192.168.64.3/api-aplikasi-android-mysql-master/";
+    public static final String URL_ADD=BASE_URL+"/tambahpgw.php";
+    public static final String URL_GET_ALL=BASE_URL+"/tampilsemuapgw.php";
+
+    public static final String TAG_JSON_ARRAY="result";
+    public static final String TAG_ID="id";
+    public static final String TAG_NAME="name";
+
+}
